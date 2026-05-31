f6317f6dfcd3d9f13e2bd2994d1f364abd5b568d
Arav2011 <monikasangrai@gmail.com>
2026-05-31 19:36:09 +0200

    Add files via upload

---
+++ sets2.py
@@ -0,0 +1,35 @@
+# importing "array" for array creations
+import array as arr
+# creating an araray with interger type
+a = arr.array('i', [1, 2, 3,])
+# printing the original array
+print("\nThe new created array is : ", end = "")
+for i in range (0,3):
+    print(a[i], end = " ")
+print()
+
+# creating an array with float type
+b = arr.array('d', [2.5, 3.2, 3.3])
+
+# printing the original array
+print("\nThe new created array is : ", end = "")
+for i in range (0,3):
+    print(b[i], end = " ")
+
+a.insert(1,4)
+
+print("\nArray after the insertion : ", end ="")
+for i in (a):
+    print(i, end = "")
+print()
+
+# adding an element using append()
+b.append(4.4)
+
+print("\nArray after insertion : ", end ="")
+for i in (b):
+    print(i, end = "")
+print()
+
+print("Access element is:", a[0])
+print("Access element is:", a[2])
